527fefae436f35ed9933dc2cb952ef66fa043cee
vle04 <ngocvinale@gmail.com>
2025-07-17 19:57:42 -0400

    basic style home menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import HomeMenu from "@/components/HomeMenu"
 export default function Home() {
   return (
     // home page content
-    <section>
+    <section className="flex justify-center items-center h-screen">
+      {/* navigation links */}
       <HomeMenu/>
     </section>
   )
--- src/components/HomeMenu.tsx
+++ src/components/HomeMenu.tsx
@@ -1,28 +1,29 @@
-"use client"
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
 
 export default function HomeMenu() {
-    return (
-        <div>
-            {/* navigation links */}
-            <div className="gap-6 text-center">
-                <Link href={"/"}>
-                <h2>home</h2>
-                </Link>
+  return (
+    <nav className="flex flex-col border min-w-[100px] h-fit px-3 py-2">
+      <h1 className="font-bold">menu</h1>
+      <hr className="my-2 w-full"></hr>
+      <div className="flex flex-col">
+        <Link href={"/"}>
+          <h2>home</h2>
+        </Link>
 
-                <Link href={"/projects"}>
-                <h2>projects</h2>
-                </Link>
+        <Link href={"/projects"}>
+          <h2>projects</h2>
+        </Link>
 
-                <Link href={"/fine-art"}>
-                <h2>fine art</h2>
-                </Link>
+        <Link href={"/fine-art"}>
+          <h2>fine art</h2>
+        </Link>
 
-                <Link href={"/photos"}>
-                <h2>photos</h2>
-                </Link>
-            </div>
-        </div>
-    )
+        <Link href={"/photos"}>
+          <h2>photos</h2>
+        </Link>
+      </div>
+    </nav>
+  );
 }
