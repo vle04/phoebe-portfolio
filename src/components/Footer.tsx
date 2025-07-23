3073199f5087c87cb7bf145a199cd35cf4477caf
vle04 <ngocvinale@gmail.com>
2025-07-22 23:30:06 -0400

    images in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,27 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import RaccoonPhone from "@/public/images/raccoon-phone.png";
+import Trio from "@/public/images/trio.png";
 
 export default function Footer() {
   return (
     <div className="flex flex-col w-screen items-center">
       {/* images container */}
-      <div>images</div>
+      <div className="flex flex-row w-full items-center justify-between">
+        <Image
+            src={RaccoonPhone}
+            alt="raccoon holding a phone"
+            width={100}
+            height={140}
+        />
+        <Image
+            src={Trio}
+            alt="mia lin phoebe yao and vina le"
+            width={260}
+            height={97}
+        />
+      </div>
 
       {/* the rest of the footer */}
       <div className="flex flex-row justify-between bg-neutral-300 w-full items-center px-[40px] h-[8rem]">
