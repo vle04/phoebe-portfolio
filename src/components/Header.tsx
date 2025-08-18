196ac5021d1cef8561a0b925423dbe8e35c152bb
vle04 <ngocvinale@gmail.com>
2025-08-17 22:01:22 -0400

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,9 @@
 // header at the top of every page (except home)
 
-import Image from "next/image";
-import Raccoon from "../../public/images/raccoon-gesture.png";
-
 export default function Header() {
   return (
-    <header className="relative flex w-screen mb-20 justify-end items-center">
-      <h1 className="text-[103px] px-[160px] relative" style={{ fontFamily: "PhoebeFont" }}>phoebe yao</h1>
-      <Image
-        src={Raccoon}
-        alt="raccoon gesturing at the title phoebe yao"
-        width={100}
-        height={100}
-        className="absolute -bottom-12 right-15"
-      />
+    <header className="relative flex w-screen mb-[20px] items-center">
+      <h1 className="text-[74px] px-[55px] mt-[20px]" style={{ fontFamily: "PhoebeFont" }}>&#x3b; Phoebe Yao &#x3b;</h1>
     </header>
   );
 }
